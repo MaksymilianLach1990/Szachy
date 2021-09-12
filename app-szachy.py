225295e84b61ac4e1013ba7a8ef4cc317f9609d3
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-09-12 10:58:55 +0200

    Add boards and markers

--- app-szachy.py
+++ app-szachy.py
@@ -5,7 +5,7 @@ import random
 from models import *
 
 # Podstawowe narzędzia i wytyczne
-los = random.randint(0.0, 255.0)
+los = random.randint(0, 255)
 t = time.time()
 pen = turtle.Turtle()
 pen.hideturtle()
@@ -14,15 +14,66 @@ pen.screen.bgcolor('white')  # Kolor tła
 pen.speed(0)  # Prędkość rysowania
 
 #  POZIOM - PUNKT STARTOWY
-#  PION - PUNKT STARTOWY
 stax = 0 - (700/2-140)
+#  PION - PUNKT STARTOWY
 stay = 0 + (700/2+10)
 
 #  SLOWNIKI
-pole_kolor = dict()
-pole_figora = dict()
-pole_plansza = []
-team_biale = {'color': 0}
-team_czarne = {'color': 1}
-figory = ['pionek', 'wierza', 'skoczek', 'goniec', 'krolowa', 'krol']
+pole_kolor = dict()  # Słownik odpowiadający za określenie kolorów pól na planszy
+pole_figora = dict()  # Słownik odpowiadający za określanie jaki pionek stoi na danym polu
+pole_plansza = []  # Lista pól na planszy
+team_biale = {'color': 0}  # Słownik zawierający pionki drużyny białej i ich pozycje
+team_czarne = {'color': 1}  # Słownik zawierający pionki drużyny czarnej i ich pozycje
+figory = ['pionek', 'wierza', 'skoczek', 'goniec', 'krolowa', 'krol']  # Lista dostępnych figór w grze
 pen.hideturtle()
+
+
+# Wypełnianie słowników odpowiednimi elementami, ustalenie odpowiednik kolorów dla konkretnych pól
+def szachownica():
+    liczba = 0
+    c = 1
+    for i in 'ABCDEFGH':
+        for n in '12345678':
+            if liczba == 8:
+                liczba = 0
+                if c == 1:
+                    c -= 1
+                elif c == 0:
+                    c += 1
+            if c == 1:
+                pole_kolor[i + n] = 0
+                pole_figora[i + n] = 0
+                team_biale[i + n] = 0
+                team_czarne[i + n] = 0
+                pole_plansza.append(i+n)
+                c -= 1
+            elif c == 0:
+                pole_kolor[i + n] = 1
+                pole_figora[i + n] = 0
+                team_biale[i + n] = 0
+                team_czarne[i + n] = 0
+                pole_plansza.append(i+n)
+                c += 1
+            liczba += 1
+
+
+# Rysowanie planszy wraz z oznaczeniami na pionowej i poziomej lini
+def plansza():
+    
+    wspolrzedne = list(pole_kolor.keys())
+    for i in wspolrzedne[0: 2]:
+        znajdz(i)
+        pole(pole_kolor[i])
+
+
+
+
+
+# Uruchomienie programu
+if __name__ == '__main__':
+    szachownica()
+    plansza()
+    pen.up()
+    print(time.time() - t)
+    pen.textinput()
+    turtle.done()
--- models.py
+++ models.py
@@ -1,11 +1,102 @@
-# Potrzebne moduły 
+# Potrzebne moduły
 import turtle
 import random
 
 # Nazwy dla funkcji
-los = random.randint(0.0, 255.0)
+los = random.randint(0, 255)
 pen = turtle.Turtle()
+#  POZIOM - PUNKT STARTOWY
+stax = 0 - (700/2-140)
+#  PION - PUNKT STARTOWY
+stay = 0 + (700/2+10)
+
 
 # Funkcje odpowiadające za wygląd graficzny
+
+# Nadawanie konkretnego koloru polom na planszy i pionką
+def kolor(color):
+    pen.color('black')
+    if color == 1:
+        pen.fillcolor('black')
+    elif color == 0:
+        pen.fillcolor('white')
+
+
+# Tworzenie pojedynczego pola. Główny element planszy
+def pole(col):
+    pen.seth(0)
+    pen.down()
+    pen.color('black')
+    if col == 1:
+        pen.fillcolor('grey')
+    elif col == 0:
+        pen.fillcolor('white')
+    pen.begin_fill()
+    for x in range(4):
+        pen.forward(80)
+        pen.right(90)
+    pen.end_fill()
+    pen.up()
+
+
+# Ustalanie pozyscji od której zacząć rysować
+def znajdz(pole_1):
+    pion = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+    poziom = '1', '2', '3', '4', '5', '6', '7', '8'
+    wartosc = 0
+    x = 0
+    y = 0
+    for w in pole_1:
+        if w in pion:
+            for i in pion:
+                if w == i:
+                    y = stay - (wartosc * 80)
+                else:
+                    wartosc += 1
+        elif w in poziom:
+            n = int(w) - 1
+            x = stax + (n * 80)
+    pen.up()
+    pen.goto(x, y)
+
+
+# Rysowanie oznaczeń pionowych, zakres liter od A do H
+def pion():
+    x = stax - 20
+    y = stay - 40
+    pen.up()
+    pen.goto(x, y)
+    for i in 'ABCDEFGH':
+        pen.write(str(i))
+        y -= 80
+        pen.goto(x, y)
+    x = stax + 660
+    y = stay - 40
+    pen.up()
+    pen.goto(x, y)
+    for i in 'ABCDEFGH':
+        pen.write(str(i))
+        y -= 80
+        pen.goto(x, y)
+
+
+# Rysowanie oznaczeń poziomych, zakres liczb od 1 do 8
+def poziom():
+    x = stax + 40
+    y = stay + 10
+    pen.up()
+    pen.goto(x, y)
+    for i in '12345678':
+        pen.write(str(i))
+        x += 80
+        pen.goto(x, y)
+    x = stax + 40
+    y = stay - 670
+    pen.up()
+    pen.goto(x, y)
+    for i in '12345678':
+        pen.write(str(i))
+        x += 80
+        pen.goto(x, y)
 
 
